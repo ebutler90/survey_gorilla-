5c5dccabe075c86ff7dd9259c2bb7002e96e0713
Eveanandi Butler & Angel Baek & Kiran <pair+ebutler90.gellieb.phanendar@devbootcamp.com>
2015-03-30 16:41:22 -0400

    Update models

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -1,4 +1,4 @@
 class Completion < ActiveRecord::Base
-  has_and_belongs_to_many: :survey_takers, class_name: "User"
-  has_and_belongs_to_many: :surveys
+  has_and_belongs_to_many :survey_takers, class_name: "User"
+  has_and_belongs_to_many :surveys
 end
