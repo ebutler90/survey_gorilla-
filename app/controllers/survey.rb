d6f3f2c6c15650f241027633c86dec30992e2919
Phanendar Chitraju <phanendar@gmail.com>
2015-03-30 17:11:17 -0400

    new controllers/new views

---
+++ app/controllers/survey.rb
@@ -0,0 +1,20 @@
+get '/survey/new' do
+  redirect '/authentication/signin' if !current_user
+  erb :'/survey/new'
+end
+
+post '/survey/new' do
+  params[:user_id] = session[:user_id]
+  @survey = Survey.create(params)
+  redirect "/survey/#{@survey.id}/edit"
+end
+
+get '/survey/:id' do
+  @survey = Survey.find(params[:id])
+  erb :'/survey/show'
+end
+
+get '/survey/:id/edit' do
+  @survey = Survey.find(params[:id])
+  erb :'/survey/edit'
+end
